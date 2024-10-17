2210791121f25f4971f50790913c09e71ba50624
Rakesh Karthik <rakeshkarthikgrk@gamil.com>
2024-10-17 09:26:03 +0000

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import {HeaderContainer, Image, DeveloperTitle} from './styledComponents'
+
+const Header = () => (
+  <HeaderContainer>
+    <Image
+      src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png"
+      alt="website logo"
+    />
+    <DeveloperTitle> - Developed By Rakesh Karthik</DeveloperTitle>
+  </HeaderContainer>
+)
+
+export default Header
---
+++ src/components/Header/styledComponents.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  background-color: #f1f5f9;
+  display: flex;
+  flex-direction: row;
+`
+
+export const Image = styled.img`
+  width: 198px;
+  margin-top: 14px;
+  margin-bottom: 14px;
+  margin-left: 48px;
+`
+export const DeveloperTitle = styled.p`
+  font-family: Roboto;
+  font-weight: bold;
+  margin-top: 60px;
+`
